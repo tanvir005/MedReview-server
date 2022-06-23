182ac62fc38bc65d985f56378017e95384b36d2c
tanvir005 <tanvir3550gmail.com>
2022-06-23 12:04:07 -1000

    cors

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
  
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wepyl.mongodb.net/?retryWrites=true&w=majority`;
