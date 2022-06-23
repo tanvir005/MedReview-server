30ea98938f8378c76fed2b521f032401b94e9b6b
tanvir005 <tanvir3550gmail.com>
2022-06-23 11:06:22 -1000

    Project initialized and simple post create

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+ 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wepyl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run(){
+    try{
+        await client.connect();
+        const postsCollection = client.db("med-review").collection("posts");
+
+
+        app.post('/posts',async(req, res) =>{
+            const post = req.body;
+            const result = await postsCollection.insertOne(post);
+
+            res.send(result);
+        });
+        app.get('/posts',async(req, res) =>{
+            const query = {};
+            const result = await postsCollection.find(query).toArray();
+
+            res.send(result);
+        })
+
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
